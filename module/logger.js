e316220947192262ff636752c7e4335d5a5762f6
Cedric <50961672+CCedDev@users.noreply.github.com>
2020-02-13 20:46:29 +0100

    + Add logger
    Add \module\logger.js
    Debug and normal logger

---
+++ module/logger.js
@@ -0,0 +1,16 @@
+const config = require('./config.json');
+
+function Debug(text, value){
+    if(config.Logger.Debug == 1){
+        console.log("[" +  new Date().toLocaleTimeString() + "]" + config.Logger.DebugPrefix + text + " : " + value);
+    }
+}
+
+function Log(text){
+    console.log("[" + new Date().toLocaleTimeString() + "]" + config.Logger.LoggerPrefix + text);
+}
+
+module.exports = {
+    Debug,
+    Log
+}
